04c76226fc92c77c1be80a4cf691059e94f6f93d
Matthias Derer <matthias.derer@esono.de>
2017-02-14 21:11:41 +0100

    implement download of archive

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
 )
 
 type rss struct {
@@ -20,6 +26,7 @@ type channel struct {
 type item struct {
 	Enclosure enclosure `xml:"enclosure"`
 	Link      string    `xml:"link"`
+	Guid      string    `xml:"guid"`
 }
 
 type enclosure struct {
@@ -27,20 +34,104 @@ type enclosure struct {
 	Type string `xml:"type,attr"`
 }
 
-type attributes struct {
-	Type string `json:"type"`
-	Url  string `json:"url"`
+type archive struct {
+	Guid []string `json:"guid"`
 }
 
 func main() {
-	xmlFile, err := ioutil.ReadFile("bestform.xml")
+
+	accountPtr := flag.String("user", "", "soup.io username")
+	flag.Parse()
+
+	if *accountPtr == "" {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	url := fmt.Sprintf("http://%s.soup.io/rss", *accountPtr)
+	feedResponse, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error fetching %s: %s", url, err)
+		os.Exit(1)
+	}
+	defer feedResponse.Body.Close()
+	feedBody, err := ioutil.ReadAll(feedResponse.Body)
+	if err != nil {
+		fmt.Printf("Error reading feed from %s: %s", url, err)
+		os.Exit(1)
 	}
 
 	var feed rss
-	xml.Unmarshal(xmlFile, &feed)
+	xml.Unmarshal(feedBody, &feed)
 
-	fmt.Printf("%q", feed)
+	a, err := readArchive()
+	if err != nil {
+		fmt.Println("No archive data found. Will create a fresh one")
+	}
 
+	for _, i := range feed.Channel.Items {
+		if inArchive(i.Guid, a) {
+			fmt.Printf("Skipping %s. Already in archive.\n", i.Enclosure.Url)
+			continue
+		}
+		response, err := http.Get(i.Enclosure.Url)
+		if err != nil {
+			fmt.Printf("Error fetching %s: %s\n", i.Enclosure.Url, err)
+			continue
+		}
+		filepath := "archive/" + path.Base(i.Enclosure.Url)
+		file, err := os.Create(filepath)
+		if err != nil {
+			fmt.Printf("Error opening file %s: %s\n", filepath, err)
+			response.Body.Close()
+			continue
+		}
+		fmt.Printf("Saving %s...\n", i.Enclosure.Url)
+		_, err = io.Copy(file, response.Body)
+		if err != nil {
+			fmt.Printf("Error writing file %s: %s\n", filepath, err)
+			response.Body.Close()
+			continue
+		}
+		response.Body.Close()
+		file.Close()
+
+		a.Guid = append(a.Guid, i.Guid)
+		saveArchive(a)
+	}
+}
+
+func saveArchive(a archive) {
+	data, err := json.Marshal(a)
+	if err != nil {
+		fmt.Println("Error marshalling archive data: ", err)
+		return
+	}
+	ioutil.WriteFile("archive/guids.json", data, 0600)
+}
+
+func inArchive(guid string, a archive) bool {
+	for _, s := range a.Guid {
+		if guid == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func readArchive() (archive, error) {
+	var a archive
+
+	archiveData, err := ioutil.ReadFile("archive/guids.json")
+	if err != nil {
+		return a, err
+	}
+
+	err = json.Unmarshal(archiveData, &a)
+	if err != nil {
+		return a, err
+	}
+
+	return a, nil
 }
