469728a253c1d5f2fe6c11f0954edc2a62481875
Matthias Derer <matthias.derer@esono.de>
2017-02-24 20:29:33 +0100

    add UnmarshalXML implementation for Attributes

--- feed/feed.go
+++ feed/feed.go
@@ -24,12 +24,11 @@ type Channel struct {
 
 // Item is one entry in the feed
 type Item struct {
-	Enclosure        Enclosure `xml:"enclosure"`
-	Link             string    `xml:"link"`
-	Guid             string    `xml:"guid"`
-	PubDate          pubDate   `xml:"pubDate"`
-	AttributesSource string    `xml:"attributes"`
-	Attributes       Attributes
+	Enclosure  Enclosure  `xml:"enclosure"`
+	Link       string     `xml:"link"`
+	Guid       string     `xml:"guid"`
+	PubDate    pubDate    `xml:"pubDate"`
+	Attributes Attributes `xml:"attributes"`
 }
 
 // Enclosure contains the url and type of the item
@@ -38,9 +37,26 @@ type Enclosure struct {
 	Type string `xml:"type,attr"`
 }
 
+// Attributes represents the json structure inside the attributes node
 type Attributes struct {
 	Type string `json:"type"`
 	Url  string `json:"url"`
+}
+
+// UnmarshalXML will parse the enclosed json and produce an Attributes element
+func (c *Attributes) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var v string
+	d.DecodeElement(&v, &start)
+
+	var attr Attributes
+	err := json.Unmarshal([]byte(v), &attr)
+	if err != nil {
+		return err
+	}
+
+	*c = attr
+
+	return nil
 }
 
 // pubDate wraps time.Time to implement the needed interface for the xml unmarshaller
@@ -54,7 +70,6 @@ func (c *pubDate) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	d.DecodeElement(&v, &start)
 	parse, err := time.Parse(time.RFC1123, v)
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 	*c = pubDate{parse}
@@ -65,29 +80,26 @@ func (c *pubDate) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 // NewFeedFromXml produces an Rss struct with the information based on the given xml
 func NewFeedFromXml(input []byte) Rss {
 	var feed Rss
-
 	xml.Unmarshal(input, &feed)
-
-	for i, item := range feed.Channel.Items {
-		source := item.AttributesSource
-		var attr Attributes
-		json.Unmarshal([]byte(source), &attr)
-		feed.Channel.Items[i].Attributes = attr
-	}
 
 	return feed
 }
 
+// localFileLister wraps the needed functions from the os package so it can be substituted in tests
 type localFileLister interface {
 	getLocalFilesInfo() ([]os.FileInfo, error)
 }
 
+// defaultLocalFileLister is a thin wrapper around the stdlib
 type defaultLocalFileLister struct{}
 
+// getLocalFilesInfo just calls ioutil.ReadDir
 func (d defaultLocalFileLister) getLocalFilesInfo() ([]os.FileInfo, error) {
 	return ioutil.ReadDir("archive")
 }
 
+// fileLister is the active implementation for file system calls. It will be substituted in tests and cannot be changed
+// from outside the package
 var fileLister localFileLister = defaultLocalFileLister{}
 
 // GetFeedUrlForUsername produces the rss feed url for a given username
