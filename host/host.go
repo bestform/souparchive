a211015350a99aac4f62247dfcd1db8824d38cf9
Matthias Derer <matthias.derer@esono.de>
2017-04-06 16:49:16 +0200

    add first working host option
    The host option will server the local archive on port 8080
    The resulting page is very simple, just listing the images
    sorted by date.

--- host/host.go
+++ host/host.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
+	"sort"
+
+	"html/template"
+
 	"github.com/bestform/souparchive/db"
 )
 
@@ -13,13 +17,22 @@ type entry struct {
 
 var localFeed []db.Item
 
+type ByTime []db.Item
+
+func (a ByTime) Len() int           { return len(a) }
+func (a ByTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByTime) Less(i, j int) bool { return a[i].Timestamp > a[j].Timestamp }
+
 // Host will host the current archive on localhost via the specified port
 func Host(port string) error {
 	archive := db.NewArchive("archive/archive.json")
 	archive.Read()
 
 	localFeed = archive.Data.Items
+	sort.Sort(ByTime(localFeed))
 
+	fs := http.FileServer(http.Dir("archive"))
+	http.Handle("/images/", http.StripPrefix("/images/", fs))
 	http.HandleFunc("/", hostList)
 
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
@@ -27,8 +40,13 @@ func Host(port string) error {
 
 func hostList(w http.ResponseWriter, r *http.Request) {
 
-	for _, item := range localFeed {
-		w.Write([]byte(item.Filename))
-		w.Write([]byte("\n"))
+	t, err := template.New("index.html").ParseFiles("host/templates/index.html")
+	if err != nil {
+		panic(err)
 	}
+	err = t.Execute(w, localFeed)
+	if err != nil {
+		panic(err)
+	}
+
 }
